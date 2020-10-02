f43e423a56cf0385b9377f08fb20cc1e70823436
Andrea <aganesh0988@gmail.com>
2020-10-01 23:35:40 -0500

    styled navLinks

--- React-backend/client/src/components/Navigation.js
+++ React-backend/client/src/components/Navigation.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import '../index.css';
 
 
 const Navigation = props =>
   <header className='navigation-container'>
+    <img
+      alt='UpwardLogo'
+      className='UpwardLogo' src='/images/UPWARDLogo.jpg' />
     <NavLink to="/" activeClassName="active">HOME</NavLink>
     <NavLink to="/users" activeClassName="active">USERS</NavLink>
     <NavLink to="/login" activeClassName="active">LOG IN</NavLink>
