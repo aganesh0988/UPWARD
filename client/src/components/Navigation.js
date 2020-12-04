06e2370f183f14241479cd2bc501eb2cdaec542b
Andrea <aganesh0988@gmail.com>
2020-12-04 16:35:17 -0600

    took off users from navbar

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -9,7 +9,7 @@ const Navigation = props =>
       alt='UpwardLogo'
       className='UpwardLogo' src='/images/UPWARDLogo.jpg' />
     <NavLink to="/" activeClassName="active">HOME</NavLink>
-    <NavLink to="/users" activeClassName="active">USERS</NavLink>
+    {/* <NavLink to="/users" activeClassName="active">USERS</NavLink> */}
     <NavLink to="/login" activeClassName="active">LOG IN</NavLink>
     <NavLink to="/signup" activeClassName="active">SIGN UP</NavLink>
     <NavLink to='/categories' activeClassName="active">CATEGORIES</NavLink>
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -34,7 +34,7 @@ function SignUp(props) {
       }),
     });
     if (response.ok) {
-      props.history.push("/cases");
+      props.history.push("/");
     }
   };
   return (
