cf8f6310594951884ca8bdfd0f166f611d086908
Andrea <aganesh0988@gmail.com>
2020-12-04 20:37:16 -0600

    installed fontawesome

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin } from '@fortawesome/free-solid-svg-icons';
+
 
 const Footer = props => {
 
@@ -6,6 +9,10 @@ const Footer = props => {
         <footer className="footer-container">
             <div className="footer-container__elements">
                 <h4>Designed by Andrea Jackson</h4>
+                <a>
+                    {/* <FontAwesomeIcon icon={faLinkedin} /> */}
+                    <i className="fab fa-linkedin"></i>
+                </a>
             </div>
         </footer>
     )
