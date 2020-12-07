e4170ef0077ffccafc5ba2520a0154a9dcb93681
Andrea <aganesh0988@gmail.com>
2020-12-07 12:35:35 -0600

    rearranged navbar

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -10,11 +10,11 @@ const Navigation = props =>
         alt='UpwardLogo'
         className='UpwardLogo' src='/images/UPWARDLogo.jpg' />
     </a>
-    <NavLink to="/" activeClassName="active">HOME</NavLink>
+    <NavLink to="/" activeClassName="active">PRODUCTS</NavLink>
     {/* <NavLink to="/users" activeClassName="active">USERS</NavLink> */}
+    <NavLink to='/categories' activeClassName="active">CATEGORIES</NavLink>
     <NavLink to="/login" activeClassName="active">LOG IN</NavLink>
     <NavLink to="/signup" activeClassName="active">SIGN UP</NavLink>
-    <NavLink to='/categories' activeClassName="active">CATEGORIES</NavLink>
   </header>
   ;
 
