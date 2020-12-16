20a6374f0d7351c18ddca2094b28bf3f0d52f9c5
Andrea <aganesh0988@gmail.com>
2020-12-15 23:47:15 -0600

    added search to navigation

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,24 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import '../index.css';
 
 
-const Navigation = props =>
-  <header className='navigation-container'>
-    <a href='/'>
-      <img
-        alt='UpwardLogo'
-        className='UpwardLogo' src='/images/UPWARDLogo.jpg' />
-    </a>
-    <NavLink to="/" activeClassName="active">PRODUCTS</NavLink>
-    {/* <NavLink to="/users" activeClassName="active">USERS</NavLink> */}
-    <NavLink to='/categories' activeClassName="active">CATEGORIES</NavLink>
-    <NavLink to="/login" activeClassName="active">LOG IN</NavLink>
-    <NavLink to="/signup" activeClassName="active">SIGN UP</NavLink>
-  </header>
-  ;
+const Navigation = props => {
 
+  const [input, setInput] = useState('')
+  const [options, setOptions] = useState()
 
+  const handleSearch = async (e) => {
+    setInput(e.target.value)
+    setOptions([])
+    const searchStr = e.target.value;
+    const data = await fetch(`/api/home/search/${searchStr}`)
+    if (data.ok) {
+      const dealerList = await data.json()
+      setOptions(dealerList.dealerships)
+    }
+  }
+
+  return (
+    <>
+      <header className='navigation-container'>
+        <a href='/'>
+          <img
+            alt='UpwardLogo'
+            className='UpwardLogo' src='/images/UPWARDLogo.jpg' />
+        </a>
+        <NavLink to="/" activeClassName="active">PRODUCTS</NavLink>
+        {/* <NavLink to="/users" activeClassName="active">USERS</NavLink> */}
+        <NavLink to='/categories' activeClassName="active">CATEGORIES</NavLink>
+        <NavLink to="/login" activeClassName="active">LOG IN</NavLink>
+        <NavLink to="/signup" activeClassName="active">SIGN UP</NavLink>
+      </header>
+    </>
+  );
+
+}
 
 
 export default Navigation;
