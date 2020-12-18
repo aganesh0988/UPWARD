2b3b48b4cd86e17003acfc56984d71de31fcd3d9
Andrea <aganesh0988@gmail.com>
2020-12-17 21:50:14 -0600

    added search bar to nav

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -8,16 +8,16 @@ const Navigation = props => {
   const [input, setInput] = useState('')
   const [options, setOptions] = useState()
 
-  // const handleSearch = async (e) => {
-  //   setInput(e.target.value)
-  //   setOptions([])
-  //   const searchStr = e.target.value;
-  //   const data = await fetch(`/api/home/search/${searchStr}`)
-  //   if (data.ok) {
-  //     const dealerList = await data.json()
-  //     setOptions(dealerList.dealerships)
-  //   }
-  // }
+  const handleSearch = async (e) => {
+    setInput(e.target.value)
+    setOptions([])
+    const searchStr = e.target.value;
+    const data = await fetch(`/api/home/search/${searchStr}`)
+    if (data.ok) {
+      const dealerList = await data.json()
+      setOptions(dealerList.dealerships)
+    }
+  }
 
   return (
     <>
@@ -28,7 +28,18 @@ const Navigation = props => {
             className='UpwardLogo' src='/images/UPWARDLogo.jpg' />
         </a>
         <NavLink to="/" activeClassName="active">PRODUCTS</NavLink>
-        {/* <NavLink to="/users" activeClassName="active">USERS</NavLink> */}
+        <div className="navlink-container__search">
+          <div className="navlink-container__search-input">
+            <input type="search" onChange={handleSearch} value={input} placeholder="Search" autoComplete="off"></input>
+          </div>
+          <div className="navlink-container__search-choices">
+            {options ? options.map(option =>
+              <div className="navlink-container__search-choices-div" key={option.id}>
+                <a className="navlink-container__search-options" href={`/dealership/${option.id}`}>{option.name}</a>
+              </div>
+            ) : null}
+          </div>
+        </div>
         <NavLink to='/categories' activeClassName="active">CATEGORIES</NavLink>
         <NavLink to="/login" activeClassName="active">LOG IN</NavLink>
         <NavLink to="/signup" activeClassName="active">SIGN UP</NavLink>
